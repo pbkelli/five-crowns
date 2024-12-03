9a92cbb6dab68334a7ee404bd3d3941cb925ee28
Kelli <kellidfrank@gmail.com>
2024-12-02 22:29:19 -0700

    Creating hand

--- src/Card.ts
+++ src/Card.ts
@@ -1,38 +1,42 @@
-import {SUIT, VALUE} from "./CardDetails";
+import { SUIT, VALUE } from "./CardDetails";
 
 export class Card {
-    private _suit: SUIT;
-    private _value: VALUE;
-    private _isWild: boolean;
+  private _suit: SUIT;
+  private _value: VALUE;
+  private _isWild: boolean;
 
-    // probably get rid of the setters
-    constructor(private cardSuit: SUIT, private cardValue: VALUE, private isCardWild = false) {
-        this._suit = cardSuit;
-        this._value = cardValue;
-        this._isWild = isCardWild;
-    }
+  // probably get rid of the setters
+  constructor(
+    private cardSuit: SUIT,
+    private cardValue: VALUE,
+    private isCardWild = false,
+  ) {
+    this._suit = cardSuit;
+    this._value = cardValue;
+    this._isWild = isCardWild;
+  }
 
-    get isWild() {
-        return this._isWild;
-    }
+  get isWild() {
+    return this._isWild;
+  }
 
-    set isWild(value) {
-        this._isWild = value;
-    }
+  set isWild(value) {
+    this._isWild = value;
+  }
 
-    get suit() {
-        return this._suit;
-    }
+  get suit() {
+    return this._suit;
+  }
 
-    set suit(value) {
-        this._suit = value;
-    }
+  set suit(value) {
+    this._suit = value;
+  }
 
-    get value() {
-        return this._value;
-    }
+  get value() {
+    return this._value;
+  }
 
-    set value(value) {
-        this._value = value;
-    }
+  set value(value) {
+    this._value = value;
+  }
 }
--- src/CardDetails.ts
+++ src/CardDetails.ts
@@ -1,21 +1,21 @@
 export enum SUIT {
-    CLUB = "CLUB",
-    HEART = "HEART",
-    SPADE = "SPADE",
-    STAR = "STAR",
-    DIAMOND = "DIAMOND"
+  CLUB = "CLUB",
+  HEART = "HEART",
+  SPADE = "SPADE",
+  STAR = "STAR",
+  DIAMOND = "DIAMOND",
 }
 
 export enum VALUE {
-    THREE = "THREE",
-    FOUR = "FOUR",
-    FIVE = "FIVE",
-    SIX = "SIX",
-    SEVEN = "SEVEN",
-    EIGHT = "EIGHT",
-    NINE = "NINE",
-    TEN = "TEN",
-    JACK = "JACK",
-    QUEEN = "QUEEN",
-    KING = "KING"
+  THREE = "THREE",
+  FOUR = "FOUR",
+  FIVE = "FIVE",
+  SIX = "SIX",
+  SEVEN = "SEVEN",
+  EIGHT = "EIGHT",
+  NINE = "NINE",
+  TEN = "TEN",
+  JACK = "JACK",
+  QUEEN = "QUEEN",
+  KING = "KING",
 }
